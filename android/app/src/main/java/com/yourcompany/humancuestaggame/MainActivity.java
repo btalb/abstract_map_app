f2d881ffde37ac8fc941d2b153beec80133f3f73
Ben Talbot <bentalbot91@gmail.com>
2017-10-29 18:25:13 +1000

    Test using Dart to call Java and native methods

--- android/app/src/main/java/com/yourcompany/humancuestaggame/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/humancuestaggame/MainActivity.java
@@ -1,14 +1,46 @@
 package com.yourcompany.humancuestaggame;
 
+import java.lang.Exception;
+
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "human.cues/hello";
+
+  static {
+    System.loadLibrary("native-lib");
+  }
+
+  public native String helloJNI();
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+        new MethodCallHandler() {
+          @Override
+          public void onMethodCall(MethodCall call, Result result) {
+            if (call.method.equals("helloJava")) {
+              result.success("Java says hello!");
+            } else if (call.method.equals("helloCpp")) {
+              try {
+                result.success(helloJNI());
+              } catch (Exception e) {
+                result.success("Java couldn't find c++");
+              }
+            } else {
+              result.notImplemented();
+            }
+          }
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(new HelloApp());
 }
 
-class MyApp extends StatelessWidget {
+class HelloApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -13,21 +16,52 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Hello Android NDK'),
+      home: new HelloPage(title: 'Hello Android NDK'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HelloPage extends StatefulWidget {
+  HelloPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _HelloPageState createState() => new _HelloPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HelloPageState extends State<HelloPage> {
+  static const MethodChannel methodChannel =
+      const MethodChannel('human.cues/hello');
+
+  static const String javaMethod = 'helloJava';
+  static const String cppMethod = 'helloCpp';
+
+  String _javaResponse = "NOPE";
+  String _cppResponse = "NOPE";
+
+  bool callCpp = false;
+
+  Future<Null> _hello() async {
+    String response;
+    try {
+      response =
+          await methodChannel.invokeMethod(callCpp ? cppMethod : javaMethod);
+    } on PlatformException {
+      response = (callCpp ? "c++" : "Java") + " didn't respond";
+    } on MissingPluginException {
+      response = "The line is blank...";
+    }
+    setState(() {
+      if (callCpp)
+        _cppResponse = response;
+      else
+        _javaResponse = response;
+    });
+
+    callCpp = !callCpp;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -42,11 +76,23 @@ class _MyHomePageState extends State<MyHomePage> {
               'Native Java, are you there?',
             ),
             new Text(
-              'NOPE',
+              _javaResponse,
+              style: Theme.of(context).textTheme.display1,
+            ),
+            new Text(
+              'Native c++, are you there?',
+            ),
+            new Text(
+              _cppResponse,
               style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(callCpp ? Icons.settings_remote : Icons.settings_phone),
+        onPressed: _hello,
+        tooltip: 'Call Java',
       ),
     );
   }
