795efdedc96080c73a60b23e219e65cd78796e8c
ben <bentalbot91@gmail.com>
2017-10-31 22:16:47 +1000

    Complete all UI elements for concept demonstration

--- app/src/main/java/com/humancues/humancuestaggame/IntroActivity.java
+++ app/src/main/java/com/humancues/humancuestaggame/IntroActivity.java
@@ -16,12 +16,19 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import java.util.Collections;
 import java.util.List;
@@ -35,6 +42,14 @@ public class IntroActivity extends AppCompatActivity {
     public String[] listExperiments = {"Experiment 1", "Experiment 2", "TODO"};
     public String[] listGoals = {"Alice's office",
             "Bob's office"};
+
+    private String currentGoal = "Alice's office";
+
+    enum TAG_TYPE { NONE, GOAL, INFO, WRONG };
+    private TAG_TYPE currentTagType = TAG_TYPE.NONE;
+    private String currentTagText = null;
+
+    private int HACK = 0;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -51,6 +66,9 @@ public class IntroActivity extends AppCompatActivity {
 
         // Configure the surface view
         configureCameraView();
+
+        // Configure the interface's interactivity
+        configureInteractivity();
     }
 
     @Override
@@ -68,6 +86,98 @@ public class IntroActivity extends AppCompatActivity {
                 return;
             }
         }
+    }
+
+    /**
+     * Configure all of the button interactivity
+     */
+    private void configureInteractivity() {
+        Button b = findViewById(R.id.start_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Pull all state from the intro screen UI
+                // TODO
+
+                // Update the UI
+                ViewSwitcher vs = findViewById(R.id.switcher);
+                vs.showNext();
+
+                ((TextView) findViewById(R.id.task_info)).setText("find " + currentGoal);
+                findViewById(R.id.background_mask).setVisibility(View.GONE);
+                updateTagDisplay();
+            }
+        });
+
+        ImageButton ib = findViewById(R.id.tag_dismiss);
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean done = currentTagType == TAG_TYPE.GOAL;
+                currentTagType = TAG_TYPE.NONE;
+                currentTagText = null;
+
+                if (done) {
+                    findViewById(R.id.background_mask).setVisibility(View.VISIBLE);
+                    ((ViewSwitcher) findViewById(R.id.switcher)).showPrevious();
+                } else {
+                    updateTagDisplay();
+                }
+            }
+        });
+
+        // TODO remove these hacks
+        SurfaceView sv = findViewById(R.id.camera_view);
+        sv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if (HACK % 3 == 0) {
+                    currentTagType = TAG_TYPE.INFO;
+                    currentTagText = "Alice's office is down the corridor, past Bob's office";
+                } else if (HACK % 3 == 1) {
+                    currentTagType = TAG_TYPE.WRONG;
+                    currentTagText = "BOB's office";
+                } else {
+                    currentTagType = TAG_TYPE.GOAL;
+                    currentTagText = "Alice's office";
+                }
+                HACK++;
+                updateTagDisplay();
+                return true;
+            }
+        });
+    }
+
+    private void updateTagDisplay() {
+        // Apply correct colours
+        int colourId;
+        switch (currentTagType) {
+            case GOAL:
+                colourId = R.color.feedbackGreen;
+                break;
+            case INFO:
+                colourId = R.color.feedbackOrange;
+                break;
+            case WRONG:
+                colourId = R.color.feedbackRed;
+                break;
+            default:
+                colourId = R.color.colorPrimary;
+        }
+        findViewById(R.id.task_panel).setBackgroundResource(colourId);
+        findViewById(R.id.tag_panel).setBackgroundResource(colourId);
+
+        // Act and exit early if tag type is NONE
+        if (currentTagType == TAG_TYPE.NONE) {
+            findViewById(R.id.tag_panel).setVisibility(View.GONE);
+            return;
+        }
+        findViewById(R.id.tag_panel).setVisibility(View.VISIBLE);
+
+        // Update the icon and text for the tag info
+        ((ImageView) findViewById(R.id.tag_dismiss)).setImageResource((currentTagType == TAG_TYPE
+                .GOAL) ? R.drawable.done_24dp : R.drawable.close_24dp);
+        ((Button) findViewById(R.id.tag_info_button)).setText(currentTagText);
     }
 
     /**
